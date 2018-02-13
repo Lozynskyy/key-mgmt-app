14585858c0ed55e5aa6ccaaa149510ce0de88da7
kalenyk1 <pasha.k.2909@gmail.com>
2018-02-13 22:34:39 +0200

    Add redux, router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './styles/index.css';
-import App from './js/components/App';
+import allReducers from './js/reducers/index';
+import Login from "./js/components/Login";
+import Dashboard from "./js/components/Dashboard";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store=createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+console.log(store.getState());
+
+ReactDOM.render(<Provider store={store}>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path='/' component={Login}/>
+                <Route path='/dashboard' component={Dashboard}/>
+            </Switch>
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById('root'));
