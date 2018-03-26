736c5b152d820eb9548c573392e3c4cbc0424356
Pavel Kalenyk <Pasha.k.2909@gmail.com>
2018-03-26 16:22:12 +0300

    local commit

--- src/js/components/SearchEmployee.js
+++ src/js/components/SearchEmployee.js
@@ -1,18 +1,29 @@
 import React from "react";
 import {reduxForm,Field} from "redux-form";
 import {max_length, required} from "../validation/user";
-import {renderField} from "./renderInputs";
+import {renderSearchField} from "./renderInputs";
 import {Button} from "react-bootstrap";
 
 class SearchEmployee extends React.Component{
     render(){
         return(
             <form onSubmit={this.props.handleSubmit}>
-                <Field name="toFind" type="text"
-                    component={renderField} label="Find by any parameter"
-                    validate={[required, max_length]}
-                />
-                <Button bsStyle="success" bsSize="small" type="submit" disabled={this.props.pristine || this.props.submitting}>Find</Button>
+                <div className="row">
+                    <div className="searchingInputsWrap">
+                        <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-6">
+
+                            <Field name="toFind" type="text"
+                                component={renderSearchField} label="Find by any parameter"
+                                validate={[required, max_length]}
+                            />
+                        </div>
+
+                        <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                            <Button bsStyle="success" bsSize="small" type="submit" disabled={this.props.pristine || this.props.submitting}>Find</Button>
+                        </div>
+                    </div>
+                </div>
+
             </form>
         );
     }
