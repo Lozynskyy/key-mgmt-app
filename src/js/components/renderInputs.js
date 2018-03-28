72820e16c93cb102904f4e3f14a04d096ba2b69c
Виталий Лозинский <vitaliylozinskiy@192-168-253-215.guest.nxc.od.ua>
2018-03-28 17:22:41 +0300

    add logout button

--- src/js/components/renderInputs.js
+++ src/js/components/renderInputs.js
@@ -9,3 +9,13 @@ export const renderField = ({input, label, type, meta: {touched, error}}) => (
         </div>
     </div>
 );
+
+export const renderSearchField = ({input, label, type, meta: {touched, error}}) => (
+    <div className="vvp-input">
+        <div className={"form-group"}>
+            <input className="form-control" {...input} placeholder={label} type={type} id={label}
+                aria-describedby={"help-block_" + label}/>
+            {touched && (error && <span id={"help-block_" + label} className="help-block">{error}</span>)}
+        </div>
+    </div>
+);
